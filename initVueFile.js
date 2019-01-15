0632cf5c2cf1aaedb61a3dff159c5a72bfb65fad
chen_yue <chen_yue@hunliji.com>
2019-01-15 21:21:36 +0800

    feat: 可以创建任何指定后缀的空文件

--- initVueFile.js
+++ initVueFile.js
@@ -2,7 +2,7 @@
  * @Author: shayne
  * @Date: 2019-01-11 16:17:32
  * @LastEditors: shayne
- * @LastEditTime: 2019-01-15 19:02:54
+ * @LastEditTime: 2019-01-15 21:21:20
  * @Desc: 命令行生成vue文件
  * 首先在package.json的scripts里添加 "init": "node generate.js"命令或者直接使用node generate
  * 再在此文件的allDirPath对象添加目录名，然后在命令行指定要在哪个目录下生成文件 
@@ -22,18 +22,20 @@ const argv = process.argv;
 const argv2 = argv[2]; // 常规情况是代表的目录名
 const argv3 = argv[3]; // 常规情况是代表的文件名
 const srcDefault = 'src'; // 默认是在src目录下
+const suffixArr = [/.js$/, /.vue$/]
 // 用来管理pages目录下的所有目录，以方便命令行生成vue模板文件
 const allDirPath = {
     c: 'components',
     p: 'pages',
-    d: 'pages/detail'
+    r: 'router'
 }
 let fileName = 'index.vue' // 文件名，默认为index
 let dirPath = allDirPath.p // 目录名,默认是src下的pages目录
 
 /**
- * 先判断argv3参数是否存在，存在则判断是否带有后缀，若不存在
- * 则默认目录路径在pages下，将argv[2]的参数作为文件名，判断其是否带后缀
+ * 先判断argv3参数是否存在，不存在
+ * 则判断argv2参数是否能匹配到allDirPath的目录，不能则作为文件名，
+ * 文件都得判断一下后缀，默认添加vue后缀，其他后缀的则会创建一个空文件
  */
 if (argv3) {
     dirPath = allDirPath[argv2]
@@ -42,7 +44,7 @@ if (argv3) {
     if (allDirPath[argv2]) {
         dirPath = allDirPath[argv2]
     } else {
-        fileName = initVueFileName(argv3)
+        fileName = initVueFileName(argv2)
     }
 }
 
@@ -51,10 +53,17 @@ if (fileExists) {
     const paths = dirPath.split('/')
     console.error(`${paths[paths.length - 1]}目录下已存在${fileName}`)
 } else {
-    fs.copy(pathResolve('template.vue'), pathResolve(fileName, dirPath), (err) => {
-        if (err) throw err;
-        console.info(`${fileName} 文件 已在${dirPath}目录下初始化完毕`)
-    })
+    if (/.vue$/.test(fileName)) {
+        fs.copy(pathResolve('template.vue'), pathResolve(fileName, dirPath), (err) => {
+            if (err) throw err;
+            console.info(`${fileName} 文件 已在${dirPath}目录下初始化完毕`)
+        })
+    } else {
+        fs.writeFile(pathResolve(fileName, dirPath), '', 'utf8', err => {
+            if (err) throw err;
+            console.info(`${fileName} 文件 已在${dirPath}目录下初始化完毕`)
+        })
+    }
 }
 
 /**
@@ -68,18 +77,18 @@ function pathResolve(filePath, dirPath) {
 }
 
 /**
- * @desc: 判断参数是不是带.vue后缀
+ * @desc: 判断参数文件名是否带指定类型的后缀
  */
-function isVue (fileName) {
-    return /.vue$/.test(fileName)
+function isFileSuffix (fileName) {
+    return suffixArr.some(suffix => suffix.test(fileName))
 }
 
 /**
  * @desc: 格式化文件名
  */
 function initVueFileName (fileName) {
-    if (!isVue(fileName)) {
+    if (!isFileSuffix(fileName)) {
         return fileName + '.vue'
-    }
+    } 
     return fileName
 }
